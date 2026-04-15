a36e3aafa970b25e80b3f2e7d9a5048c83eedfd7
yeonhwan <yeonhwan619@gmail.com>
2026-04-15 15:52:53 +0900

    add unwind package and remove default index

---
+++ client/metro.config.js
@@ -0,0 +1,9 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { withUniwindConfig } = require("uniwind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withUniwindConfig(config, {
+  cssEntryFile: "./globals.css",
+  dtsFile: "./app/uniwind-types.d.ts",
+});
